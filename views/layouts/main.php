72d294892e2efce79108e6ccef4e4a53f8577910
MikhalevValentin <valmih88@gmail.com>
2019-10-03 20:08:43 +0300

    fixed logo

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,7 +32,7 @@ AppAsset::register($this);
     <div class="header-column-wrap">
         <div class="row">
             <div class="column header-column">
-                <a href="https://skrepkaexpo.ru" class="logo">
+                <a href="." class="logo">
                     <img src="./img/logo.png" alt="Logo">
                 </a>
             </div>
